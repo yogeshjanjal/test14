2958f194ac70d8a15f4846c8b61146535826bd5e
yogijanjal <yogijanjal123@gmail.com>
2023-10-24 14:40:12 +0530

    my first tf commit

---
+++ main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+module "ec2_instance" {
+  source = "./modules/ec2_instance"
+  ami = var.ami
+  instance_type = lookup(var.instance_type, terraform.workspace, "t2.micro")
+}
